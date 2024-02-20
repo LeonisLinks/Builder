9ed2babde39d21426421285e5f28862ff4c44222
oriionn <contact@oriondev.fr>
2024-02-20 23:29:31 +0100

    Ajout de la lecture du fichier config et commencement des ajouts de class pour le body

--- app.js
+++ app.js
@@ -17,7 +17,27 @@ const spinners = [ora(`Loading ${input}`).start()];
 let parsedData;
 
 try {
-	let data = fs.readFileSync(input, 'utf8');
+	let data = fs.readFileSync(input, { encoding: 'utf8' });
+	parsedData = yaml.load(data);
+
+	if (!parsedData) return spinners[0].fail(`Error loading ${input}: No data found`);
+	if (parsedData instanceof Error) return spinners[0].fail(`Error loading ${input}: ${parsedData.message}`);
+	if (!parsedData.name || !parsedData.description || !parsedData.background) return spinners[0].fail(`Error loading ${input}: Invalid data`);
+
+	spinners[0].succeed(`Loaded ${input}`);
 } catch(e) {
 	spinners[0].fail(`Error loading ${input}`);
+}
+
+// Parsing data
+spinners.push(ora('Parsing data').start());
+
+let bodyClass = "";
+
+if (parsedData.effects.background === "night_time") {
+	bodyClass = "nighttime";
+} else if (parsedData.effects.background === "old_tv") {
+	bodyClass = "oldtv";
+} else if (parsedData.effects.back) === "blurred") {
+	bodyClass = "blurred";
 }
